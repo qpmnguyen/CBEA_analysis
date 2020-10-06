533f02545f639349914d4de901f743f983957ab5
Quang Nguyen <quangpmnguyen@gmail.com>
2020-10-05 23:43:17 -0400

    data_creation.R final.docx

--- R/data_creation.R
+++ R/data_creation.R
@@ -7,7 +7,7 @@ library(glue)
 source("simulations.R")
 
 sim <- list(
-  rep = seq(1,10),
+  rep = seq(1,100),
   b_spar = c(0.2, 0.4, 0.6, 0.8),
   b_rho = c(0.1, 0.2, 0.5),
   n_inflate = c(50,100,150,200)
@@ -24,8 +24,8 @@ with_progress({
   p <- progressor(steps = nrow(sim))
   sim$sim <- furrr::future_map(sim$param, ~{
     p()
-    zinb_simulation(n_samp = 200, b_spar = .x$b_spar, b_rho = .x$b_rho, 
-                    eff_size = 1, n_inflate = .x$n_inflate, rho_ratio = 1, n_tax = 300, method = "normal")
+    zinb_simulation(n_samp = 1000, b_spar = .x$b_spar, b_rho = .x$b_rho, 
+                    eff_size = 1, n_inflate = .x$n_inflate, rho_ratio = 1, n_tax = 1000, method = "normal")
   }, .options = opt)
 })
 plan(sequential)
