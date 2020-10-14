4f6254a8c0121c39aaaab4095f0568bf725da63f
Quang Nguyen <quangpmnguyen@gmail.com>
2020-10-14 11:48:54 -0400

    Adding new labels to external files and then modularize null_hmp_data.R

--- R/null_hmp_data.R
+++ R/null_hmp_data.R
@@ -11,37 +11,35 @@ source("../R/utils.R")
 source("../R/diff_abundance.R")
 
 option_list <- list(
-  make_option("--ncores", type="integer", help="Number of cores to run parallel jobs on")
+  make_option("--data_path", type = "character", help = "Path to the data set to be loaded"),
+  make_option("--ncores", type="integer", help="Number of cores to run parallel jobs on"),
+  make_option("--method", type ="character", help="What methods to evaluate"),
+  make_option("--label_path", type = "character", help = "Path to label file")
 )
 
 opt <- parse_args(OptionParser(option_list=option_list))
 
-data <- qread(file = "../data/hmp_stool_16S.qs")
-labels <- matrix(nrow = nsamples(data), ncol = 1000)
-for (i in 1:ncol(labels)){
-  labels[,i] <- rbinom(nsamples(data),1,0.5)
+path <- opt$data_path
+data_name <- strsplit(path, "/")[[1]]
+dir_name <- strsplit(data_name[length(data_name)],".", fixed = T)[[1]][1]
+
+if (!dir.exists(dir_name)){
+  dir.create(dir_name)
 }
 
-methods <- c("cilr_wilcox", "cilr_welch", "gsva_wilcox", "gsva_welch", 
-                                 "deseq2", "corncob", "clr_wilcox", "clr_welch")
+method <- opt$method
+data <- qread(file = path)
+labels <- qread(file = opt$label_path)
 
-for (i in 1:length(methods)){
-  print(methods[i])
-  if (file.exists(glue("./type_i_16S_{method}.qs", method = methods[i]))){
-    print("Moving to the next iteration")
-    next
-  } else {
-    plan(multiprocess, workers = opt$ncores)
-    results <- future_map_dfc(1:ncol(labels), .f = ~{
-      temp <- data
-      sample_data(temp)$group <- labels[,.x]
-      physeq_eval(temp, method = methods[i], agg_level = "GENUS")
-    }, .progress = T)
-    name <- glue("./type_i_16S_{method}.qs", method = methods[i])
-    qsave(results, file = name)
-    plan(sequential)
-  }
-}
+plan(multiprocess, workers = opt$ncores)
+results <- future_map_dfc(1:ncol(labels), .f = ~{
+  temp <- data
+  sample_data(temp)$group <- labels[,.x]
+  physeq_eval(temp, method = method, agg_level = "GENUS")
+}, .progress = T)
+output_name <- glue("{dir}/type_i_{method}.qs", method = method, dir = dir_name)
+qsave(results, file = output_name)
+plan(sequential)
 
 
 
