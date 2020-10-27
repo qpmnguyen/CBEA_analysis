a9d8430234e5910c4bacbd458433c6216b986dfa
Quang Nguyen <quangpmnguyen@gmail.com>
2020-10-27 12:44:20 -0400

    New auc script that fixes the right qs file load and adding support for cdf and zscore implementations of cilr

--- R/auc_classification_rankings.R
+++ R/auc_classification_rankings.R
@@ -17,69 +17,99 @@ option_list <- list(
 opt <- parse_args(OptionParser(option_list=option_list))
 
 print("Loading parameters....")
-parameters <- readRDS(file = "objects/auc_sim/parameters.rds")
+parameters <- qread(file = "auc_sim/parameters.qs")
 cores <- opt$ncores
+
 print("Estimating cilr scores")
 tic()
-plan(multicore, workers = cores)
+plan(multisession, workers = cores)
 with_progress({
   p <- progressor(steps = nrow(parameters))
   parameters$scores_cilr <- future_map(1:nrow(parameters), .f = ~{
     p()
-    data <- readRDS(file = glue("objects/auc_sim/simulation_{.x}"))
-    simple_cilr(X = data$X, A = data$A, preprocess = T, pcount = 1)
+    data <- qread(file = glue("auc_sim/simulation_{.x}.qs"))
+    simple_cilr(X = data$X, A = data$A, preprocess = T, pcount = 1, resample = F)
+  }, .progress = TRUE)
+})
+plan(sequential)
+toc()
+
+print("Estimating cilr scores with zscore implementation")
+tic()
+plan(multisession, workers = cores)
+with_progress({
+  p <- progressor(steps = nrow(parameters))
+  parameters$scores_cilr <- future_map(1:nrow(parameters), .f = ~{
+    p()
+    data <- qread(file = glue("auc_sim/simulation_{.x}.qs"))
+    simple_cilr(X = data$X, A = data$A, preprocess = T, pcount = 1, resample = T, method = "zscore")
+  }, .progress = TRUE)
+})
+plan(sequential)
+toc()
+
+print("Estimating cilr scores with cdf implementation")
+tic()
+plan(multisession, workers = cores)
+with_progress({
+  p <- progressor(steps = nrow(parameters))
+  parameters$scores_cilr <- future_map(1:nrow(parameters), .f = ~{
+    p()
+    data <- qread(file = glue("auc_sim/simulation_{.x}.qs"))
+    simple_cilr(X = data$X, A = data$A, preprocess = T, pcount = 1, resample = T, method = "cdf")
   }, .progress = TRUE)
 })
 plan(sequential)
 toc()
 
 print("Estimating GSVA Pois scores")
-plan(multicore, workers = cores)
+plan(multisession, workers = cores)
 with_progress({
   p <- progressor(steps = nrow(parameters))
   parameters$scores_gsva_pois <- future_map(1:nrow(parameters), .f = ~{
     p()
-    data <- readRDS(file = glue("objects/auc_sim/simulation_{.x}"))
+    data <- qread(file = glue("auc_sim/simulation_{.x}.qs"))
     generate_alt_scores(X = data$X, A = data$A, method = "gsva", preprocess = T, transform=NULL, pcount=1)
   }, .progress = TRUE)
 })
 plan(sequential)
 
 print("Estimating GSVA Gauss scores")
-plan(multicore, workers = cores)
+plan(multisession, workers = cores)
 with_progress({
   p <- progressor(steps = nrow(parameters))
   parameters$scores_gsva_gauss <- future_map(1:nrow(parameters), .f = ~{
     p()
-    data <- readRDS(file = glue("objects/auc_sim/simulation_{.x}"))
+    data <- qread(file = glue("auc_sim/simulation_{.x}.qs"))
     generate_alt_scores(X = data$X, A = data$A, method = "gsva", preprocess = T, transform="clr", pcount=1)
   },.progress = TRUE)
 })
 plan(sequential)
 
 print("Estimating GSEA scores")
-plan(multicore, workers = cores)
+plan(multisession, workers = cores)
 with_progress({
   p <- progressor(steps = nrow(parameters))
   parameters$scores_ssgsea <- future_map(1:nrow(parameters), .f = ~{
     p()
-    data <- readRDS(file = glue("objects/auc_sim/simulation_{.x}"))
+    data <- qread(file = glue("auc_sim/simulation_{.x}.qs"))
     generate_alt_scores(X = data$X, A = data$A, method = "ssgsea", preprocess = T, transform=NULL, pcount=1)
   }, .progress = TRUE)
 })
 plan(sequential)
 
 print("Estimating proportional counts as scores")
-plan(multicore, workers = 5)
+plan(multisession, workers = cores)
 with_progress({
   p <- progressor(steps = nrow(parameters))
   parameters$scores_prop <- future_map(1:nrow(parameters), .f = ~{
     p()
-    data <- readRDS(file = glue("objects/auc_sim/simulation_{.x}"))
+    data <- qread(file = glue("auc_sim/simulation_{.x}.qs"))
     generate_alt_scores(X = data$X, A = data$A, method = "prop", preprocess = T, transform="prop", pcount=1)
   })
 })
 plan(sequential)
+
 
 print("Gathering all results into one file...")
 scores <- parameters %>% dplyr::select(starts_with("scores"))
@@ -87,7 +117,7 @@ auc <- vector(mode = "list", ncol(scores)-1)
 for (i in 2:ncol(scores)){
   new_name <- glue("auc_{name}", name = colnames(scores)[i])
   values <- map_dbl(1:nrow(scores), .f = function(.x){
-    data <- readRDS(file = glue("objects/auc_sim/simulation_{idx}",idx =.x))
+    data <- qread(file = glue("auc_sim/simulation_{idx}.qs",idx =.x))
     stat <- calculate_statistic(eval = "auc", pred = scores[.x,i][[1]],
                                 true = data$label)
     return(stat)
