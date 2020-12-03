5d74fd9c790eee96c2aede0f6e86e7bdf23c3cf8
Quang Nguyen <quangpmnguyen@gmail.com>
2020-12-03 17:21:39 -0500

    auc_classification_update

--- R/auc_classification.R
+++ R/auc_classification.R
@@ -37,10 +37,10 @@ sim$eval <- future_map(1:nrow(sim), .f = ~{
     source("../R/cilr.R")
     data <- readRDS(file = glue("{dir}/simulation_{i}.rds", 
                             dir = dir, i = sim$id[.x]))
-    if (distr %in% c("gsva", "ssgsea")){
+    if (sim$distr[.x] %in% c("gsva", "ssgsea")){
         scores <- generate_alt_scores(X = data$X, A = data$A, method = sim$distr[.x], 
                                     preprocess = T, transform=NULL, pcount=1)
-    } else if (distr == "none"){
+    } else if (sim$distr[.x] == "none"){
         scores <- cilr(X = data$X, A = data$A, resample = F, output = "cdf")        
     } else {
         scores <- cilr(X = data$X, A = data$A, resample = T, 
