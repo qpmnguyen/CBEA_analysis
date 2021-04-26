f67275cef63a3c947f55d3de10af678375574049
Quang Nguyen <quangpmnguyen@gmail.com>
2021-04-26 18:32:57 -0400

    Fix names so that they sound a bit better

--- analyses/simulations_diff_ab/_targets.R
+++ analyses/simulations_diff_ab/_targets.R
@@ -32,7 +32,7 @@ sim_grid <- cross_df(list(
 sim_grid$id <- seq(1, nrow(sim_grid))
 saveRDS(sim_grid, file = "output/sim_diff_ab_grid.rds")
 
-sim_eval_grid <- tar_target(sim_eval_grid, {
+eval_grid <- tar_target(sim_eval_grid, {
   eval_settings <- cross_df(list(
     model = c("cilr_wilcox", "cilr_welch"),
     distr = c("mnorm", "norm"),
@@ -51,29 +51,29 @@ sim_eval_grid <- tar_target(sim_eval_grid, {
 # define function that performs the differential abundance testing across simulation grid
 # define a function that performs evaluation across the differential abundance results  
 analysis <- tar_map(values = sim_grid, unlist = TRUE, names = c("id"), 
-        tar_target(simulation, {
+        tar_target(simulation_dat, {
             zinb_simulation(n_samp = n_samp, spar = spar, s_rho = s_rho, eff_size = eff_size, 
                             n_inflate = n_inflate, n_sets = n_sets, prop_set_inflate = prop_set_inflate, 
                             method = method, samp_prop = samp_prop, vary_params = vary_params, n_tax = n_tax, 
                             prop_inflate = prop_inflate)
         }),
         tar_target(transform_dat, {
-            sim2phylo(simulation)
+            sim2phylo(simulation_dat)
         }),
-        tar_target(analysis,{
+        tar_target(analysis_res,{
             print(sim_eval_grid)
-            diff_ab(transform, method = sim_eval_grid$model, 
+            diff_ab(transform_dat, method = sim_eval_grid$model, 
                     agg_level = "GENUS", data_type = "16S", prune = FALSE, 
                     adj = sim_eval_grid$adj, distr = sim_eval_grid$distr, 
                     output = sim_eval_grid$output)
         }, pattern = map(sim_eval_grid)),
-        tar_target(evaluation,{
-            res <- eval_function(analysis)
+        tar_target(evaluation_res,{
+            res <- eval_function(analysis_res)
             dplyr::bind_cols(id = id, sim_eval_grid, est = res)
         })
 )
 
 combined <- tar_combine(combined_results, analysis[[4]])
-save_file <- tarchetypes::tar_rds(save_file, saveRDS(combined_results, file = "output/sim_diff_ab.rds"))
+file <- tarchetypes::tar_rds(save_file, saveRDS(combined_results, file = "output/sim_diff_ab.rds"))
 
-list(sim_eval_grid, analysis, combined, save_file)
+list(eval_grid, analysis, combined, file)
