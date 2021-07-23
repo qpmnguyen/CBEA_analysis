c9785f34b38248772d028f86951fc95cf8269b1f
Quang Nguyen <quang.p.nguyen.gr@dartmouth.edu>
2021-07-23 09:29:27 -0400

    Plotting performance values

--- R/plotting_diff_ab_sim.R
+++ R/plotting_diff_ab_sim.R
@@ -41,16 +41,23 @@ combined_df <- combined_df %>% group_by(model, distr, adj, output, spar, eff_siz
 type_i_error <- combined_df %>% filter(`Effect Size` == 1)
 power <- combined_df %>% filter(`Effect Size` > 1)
 
-type_i_plt <- ggplot(type_i_error, aes(x = model, y = est, shape = adj, linetype = output)) + 
-    geom_pointrange(aes(ymin = lower, ymax = upper, col = model), show.legend = FALSE, position = position_dodge(width = 0.5)) +  
-    facet_grid(Sparsity ~ Correlation, labeller = label_both) + geom_hline(aes(yintercept = 0.05), col = "red") + 
+# type_i_plt <- ggplot(type_i_error, aes(x = model, y = est, shape = adj, linetype = output)) + 
+#     geom_pointrange(aes(ymin = lower, ymax = upper, col = model), show.legend = FALSE, position = position_dodge(width = 0.5)) +  
+#     facet_grid(Sparsity ~ Correlation, labeller = label_both) + geom_hline(aes(yintercept = 0.05), col = "red") + 
+#     scale_color_d3() + theme_bw() + 
+#     theme(axis.title.x = element_blank()) + 
+#     labs(y = "Type I error", linetype = "Output type", shape = "Correlation adjustment", col = "Model")
+type_i_plt <- ggplot(type_i_error, aes(x = Sparsity, y = est, col = model, shape = adj)) + 
+    geom_pointrange(aes(ymin = lower, ymax = upper, linetype = output)) + 
+    geom_line(aes(linetype = output), show.legend = FALSE) + 
+    facet_grid(~ Correlation, labeller = label_both) + 
     scale_color_d3() + theme_bw() + 
-    theme(axis.title.x = element_blank()) + 
-    labs(y = "Type I error", linetype = "Output type", shape = "Correlation adjustment", col = "Model")
+    ylim(c(0.03, 0.12)) + 
+    labs(y = "Type I error", linetype = "Output type", shape = "Correlation adjustment", col = "Model") + 
+    geom_hline(yintercept = 0.05, col = "red")
 
 ggsave(type_i_plt, filename = "figures/sim_diff_ab_type_i_error.png", dpi = 300, width = 15, height = 8)
 ggsave(type_i_plt, filename = "figures/sim_diff_ab_type_i_error.eps", dpi = 300, width = 15, height = 8)
-
 
 pwr_plot <- ggplot(power, aes(x = Sparsity, y = est,  col = model, shape = adj)) +
     geom_pointrange(aes(ymin = lower, ymax = upper, linetype = output)) + 
@@ -61,14 +68,16 @@ pwr_plot <- ggplot(power, aes(x = Sparsity, y = est,  col = model, shape = adj))
 ggsave(pwr_plot, filename = "figures/sim_diff_ab_pwr.png", dpi = 300, width = 10, height = 8)
 ggsave(pwr_plot, filename = "figures/sim_diff_ab_pwr.eps", dpi = 300, width = 10, height = 8)
 
-combo_plot <- type_i_plt + pwr_plot + plot_annotation(tag_levels = "A") + 
-    plot_layout(widths = c(0.7,0.3), guides = "collect") & 
+combo_plot <- type_i_plt + pwr_plot + plot_annotation(tag_levels = list(c("A. Type I error", "B. Power"))) + 
+    plot_layout(guides = "collect") & 
     guides(linetype = guide_legend(override.aes = list(shape = NA)), 
            color = guide_legend(override.aes = list(linetype = 0)), 
            shape = guide_legend(override.aes = list(linetype = 0))) &
-    theme(legend.position = "bottom", legend.box = "horizontal", legend.direction = "horizontal", 
-          legend.justification = "center", legend.box.just = "center", 
-          legend.box.margin = margin(r = -12, l = -12, unit = "cm"))
+    theme(plot.tag = element_text(face = "bold", size = 16, hjust = 0), 
+          legend.position = "bottom", legend.box = "horizontal", 
+          legend.direction = "horizontal", legend.justification = "center", 
+          legend.box.just = "center", legend.box.margin = margin(r = -12, l = -12, unit = "cm"), 
+          plot.tag.position = c(0,1.05), plot.margin = margin(t = 22, b = 15, l = 10))
 
 ggsave(combo_plot, filename = "figures/sim_diff_ab_comb.png", dpi = 300, width = 18, height = 10)
 ggsave(combo_plot, filename = "figures/sim_diff_ab_comb.eps", dpi = 300, width = 18, height = 10)
