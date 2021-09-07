1d3615007e6a561b631d4112e0fdf6037eaa2dd9
Quang Nguyen <quangpmnguyen@gmail.com>
2021-09-07 10:51:42 -0400

    redo-ing p_val distribution

--- R/p_values.R
+++ R/p_values.R
@@ -37,20 +37,21 @@ if (!file.exists("additional_files/pval_distr.rds")){
 
 
 grid <- grid_eval %>% unnest(model) %>% rename(pval = Set1ss) %>% 
-    select(-data)
+    dplyr::select(-data)
 
 grid <- grid %>% rename(Sparsity = spar, Correlation = s_rho) %>% 
     mutate(distr = recode(distr, "mnorm" = "Mixture Normal", "norm" = "Normal"))
 
 pval_plot <- ggplot(grid, aes(col = distr, sample = pval)) + 
     facet_grid(Sparsity ~ Correlation, labeller = label_both) +
-    stat_qq(distribution = qunif, alpha = 0.01) + 
+    stat_qq(distribution = qunif) + 
     stat_qq_line(distribution = qunif, linetype = "dashed") + 
     theme_bw() + scale_color_d3() + 
     guides(color = guide_legend(override.aes = list(alpha = 1))) + 
     labs(x = "Theoretical", y = "Sample", col = "Distribution")
 ggsave(pval_plot, filename = "figures/pval_distr.png", dpi = 300, width = 8, height = 6)
-ggsave(pval_plot, filename = "figures/pval_distr.eps", device = cairo_ps, dpi = 300, width = 8, height = 6)
+ggsave(pval_plot, filename = "figures/pval_distr.eps", 
+       dpi = 300, width = 8, height = 6)
 
 file.copy(from = Sys.glob("figures/*.png"), to = glue("{save_dir}", dir = save_dir), 
           recursive = TRUE, overwrite = TRUE)
