ac20abf838d5a3e117fa00dee020de4151dafdac
Quang Nguyen <quang.p.nguyen.gr@dartmouth.edu>
2022-01-10 11:12:56 -0500

    Adding new results

--- R/functions_data_diffab.R
+++ R/functions_data_diffab.R
@@ -2,40 +2,9 @@ library(DESeq2)
 library(corncob)
 library(glue)
 library(binom)
+library(CBEA)
 
-#' Generate scores
-#' @param set BiocSet
-#' @param method Either ssgsea, gsva, wilcoxon or auc_scores
-#' @param metric Either auc or inference
-#' @param ... Additional arguments passed on to CBEA
-enrichment_analysis <- function(physeq, set, method, 
-                                label = NULL, metric, preprocess = TRUE, ...) {
-    if (method %in% c("gsva", "wilcoxon")){
-        preprocess <- FALSE
-        message("Forcing preprocess to be false when evaluating GSVA")
-    }
-    if (preprocess == TRUE){
-        physeq <- transform_sample_counts(physeq, function(x) x + 1)
-        physeq <- transform_sample_counts(physeq, function(x) x / sum(x))
-    }
-    if (method %in% c("ssgsea", "gsva")) {
-        scores <- alt_scores_physeq(physeq, set, method = method, preprocess = TRUE)
-        scores <- scores %>% rownames_to_column(var = "sample_id") %>% as_tibble()
-    } else if (method == "wilcoxon") {
-        # wilcoxon with raw counts but adding pseudocounts to avoid zeroes 
-        physeq <- transform_sample_counts(physeq, function(x) x + 1)
-        if (metric == "auc") {
-            output <- "scores"
-        } else {
-            output <- "sig"
-        }
-        scores <- wc_test_physeq(
-            physeq = physeq, set = set,
-            thresh = 0.05, alt = "two.sided",
-            preprocess = TRUE, ...
-        )
-    } else if (method == "cbea") {
-        scores <- CBEA::cbea(obj = physeq, set = set, thresh = 0.05, ...)
-    }
-    return(scores)
-}
+# retrieving 
+source("R/functions_data_ss.R")
+source("R/utils.R")
+
--- script_data_diffab.R
+++ script_data_diffab.R
@@ -4,5 +4,39 @@ library(tarchetypes)
 library(tidyverse)
 library(future)
 library(phyloseq)
+source("R/functions_data_diffab.R")
 
+
+fdr <- tar_map(unlist = FALSE, values = get_settings("sig"), 
+               tar_target(index_batch, seq_len(10)),
+               tar_target(index_rep, seq_len(1)),
+               tar_target(input_data, {gingival_load()$physeq}),
+               tar_target(rand_set, {
+                   purrr::map(index_rep, ~get_rand_sets(input_data, size = size, n_sets = 1))
+               }, pattern = map(index_batch)),
+               tar_target(enrich_test, {
+                   purrr::map(rand_set, ~enrichment_analysis(input_data, 
+                                                             set = .x, method = models, 
+                                                             metric = "fdr", distr = distr, 
+                                                             adj = adj, output = "sig"))
+               }, pattern = map(rand_set)),
+               tar_target(enrich_eval, {
+                   purrr::map_dfr(enrich_test, ~{
+                       tibble(
+                           models = models,
+                           distr = distr,
+                           adj = adj,
+                           size = size,
+                           res = sum(.x[,2])/nrow(.x)
+                       )
+                   })
+               }, pattern = map(enrich_test))
+)
+
+
+
+fdr_summary <- tar_combine(combine_fdr, fdr[[6]], command = dplyr::bind_rows(!!!.x))
+
+fdr_save <- tarchetypes::tar_rds(save_fdr, saveRDS(combine_fdr, 
+                                                   "output/fdr_ss_randset.rds"))
 
